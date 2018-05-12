6d0a9e12f355e8f7fae568acf59838136f7ae22d
Wojciech Bulaty <wojciech.bulaty@gmail.com>
2018-05-12 20:28:26 +0100

    server

--- grpc-shop/proto.sh
+++ grpc-shop/proto.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-protoc --java_out=shop-backend/src/main/java/ shop-backend/src/main/resources/order.proto
+PROTO_HOME=$HOME/opt/protobuf-3.5.1
+$PROTO_HOME/bin/protoc --java_out=shop-backend/src/main/java/ shop-backend/src/main/resources/order-service.proto
--- grpc-shop/shop-backend/src/main/java/com/trafficparrot/examples/shop/ShopBackendServer.java
+++ grpc-shop/shop-backend/src/main/java/com/trafficparrot/examples/shop/ShopBackendServer.java
@@ -1,7 +1,42 @@
 package com.trafficparrot.examples.shop;
 
-public class ShopBackendServer {
-    public static void main(String[] args) {
+import com.trafficparrot.examples.shop.proto.Item;
+import com.trafficparrot.examples.shop.proto.OrderGrpc;
+import com.trafficparrot.examples.shop.proto.OrderStatus;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
 
+import java.io.IOException;
+
+public class ShopBackendServer {
+    private Server server;
+
+    private void start() throws IOException, InterruptedException {
+        int port = 12345;
+        server = ServerBuilder.forPort(port)
+                .addService(new OrderGrpc.OrderImplBase() {
+                    @Override
+                    public void purchase(Item request, StreamObserver<OrderStatus> responseObserver) {
+                        System.out.println("Purchase request = " + request);
+                    }
+                })
+                .build()
+                .start();
+        System.out.println("Server started on localhost:" + port);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                System.out.println("Shutting down gRPC server since JVM is shutting down");
+                server.shutdown();
+                System.out.println("Server shut down!");
+            }
+        });
+        server.awaitTermination();
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ShopBackendServer server = new ShopBackendServer();
+        server.start();
     }
 }
